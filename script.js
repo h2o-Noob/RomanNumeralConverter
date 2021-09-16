b28398b676198b9472a7feec71dd0771347ec501
h2o-Noob <arindamraina7743@gmail.com>
2021-09-16 16:19:36 +0530

    reverse feature#1 16-09-21

--- script.js
+++ script.js
@@ -1,8 +1,91 @@
-var btn = document.getElementById("convert")
+var conv = document.getElementById("convert")
+var rev = document.getElementById("reverse")
+var count = 3
 
-btn.addEventListener("click", function(e) {
+rev.addEventListener('click', reverse)
+function reverse()
+{
+  count++
+
+  if(count%2==0)
+  {
+    document.getElementById('heading').innerHTML = "Enter your roman number"
+
+    conv.addEventListener('click', function(e){
+    
+    let input = document.getElementById("decimal").value
+      function x(){
+        return input = input.replace("IV", 'q').replace('IX', 'w').replace("XL", 'e').replace('XC', 'r').replace("CD", 't').replace('CM', 'y').replace("M_V", 'u').replace('M_X', 'o').replace("_X_L", 'p').replace('_X_C', 'k').replace("_C_D", 'j').replace('_C_M', 'h')}
+          x(input)
+
+          let romanNumObj = {
+              "I": 1,
+              "q": 4,
+              "V": 5,
+              "w": 9,
+              "X": 10,
+              "e": 40,
+              "L": 50,
+              "r": 90,
+              "C": 100,
+              "t": 400,
+              "D": 500,
+              "y": 900,
+              "M": 1000,
+              "u": 4000,
+              "v": 5000,
+              "o": 9000,
+              "x":10000,
+              "p":40000,
+              "l":50000,
+              "k":90000,
+              "c":100000,
+              "j":400000,
+              "d":500000,
+              "h":900000,
+              "m":1000000  
+            }
+
+            let romNums =[]
+
+          for (let i=0; i<input.length; i++)
+          {
+              romNums.push(input[i])
+          }
+          console.log(romNums)
+
+          let romanKeys = Object.keys(romanNumObj)
+          let romanValues = Object.values(romanNumObj)
+
+          let value = []
+          for (let i=0; i<romNums.length; i++)
+          {
+              let index = romanKeys.indexOf(romNums[i])
+              value.push(romanValues[index])
+              console.log(value)
+          }
+          var result = 0
+          for (let i=0; i<value.length; i++)
+          {
+              result += value[i]
+          }
+          console.log(result)
+          document.getElementById("roman").value = result
+
+    })
+  }
+
+  else
+  {
+    document.getElementById('heading').innerHTML = "Enter your number"
+  }
+
+}
+
+
+conv.addEventListener("click", function(e) {
     var deci = document.getElementById("decimal").value
-    if(/^\d+$/.test(deci))
+    if(/^\d+$/.test(deci) && count%2==1)
     {
       let romanNumObj = {
         "I": 1,
@@ -18,18 +101,18 @@ btn.addEventListener("click", function(e) {
         "D": 500,
         "CM": 900,
         "M": 1000,
-        "M_V": 4000,
-        "_V": 5000,
-        "M_X": 9000,
-        "_X":10000,
-        "_X_L":40000,
-        "_L":50000,
-        "_X_C":90000,
-        "_C":100000,
-        "_C_D":400000,
-        "_D":500000,
-        "_C_M":900000,
-        "_M":1000000  
+        "Mv": 4000,
+        "v": 5000,
+        "Mx": 9000,
+        "x":10000,
+        "xl":40000,
+        "l":50000,
+        "xc":90000,
+        "c":100000,
+        "cd":400000,
+        "d":500000,
+        "cm":900000,
+        "m":1000000  
       }
       
       let romanKeys = Object.keys(romanNumObj)
@@ -54,11 +137,11 @@ btn.addEventListener("click", function(e) {
     }
     else
     {
-      alert("Please enter a valid number")
+      /*alert("Please enter a valid number")
       document.getElementById("decimal").value = ""
-      document.getElementById("roman").value = ""
+      document.getElementById("roman").value = ""*/
     }
-    })
+  })
 
 var reset = document.getElementById("reset")
 reset.addEventListener("click", function(e) {
