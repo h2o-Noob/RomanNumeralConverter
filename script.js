59bf6a80ba0cb33d8b3a486f6edb67ba0a67c710
h2o-Noob <arindamraina7743@gmail.com>
2021-09-26 10:50:32 +0530

    reverse #3(incorrect representaion fix) 26-09-21

--- script.js
+++ script.js
@@ -1,49 +1,113 @@
-var conv = document.getElementById("convert")
-var rev = document.getElementById("reverse")
-var count = 3
+var conv = document.getElementById("convert");
+var rev = document.getElementById("reverse");
+var reset = document.getElementById("reset")
+let count = 2;
 
-rev.addEventListener('click', reverse)
-function reverse()
-{
-  count++
-
-  if(count%2==0)
+rev.addEventListener("click", function (e) {
+  count += 1;
+  if(count%2===0)
+  {
+    document.getElementById('heading').innerHTML = "Enter your number"
+  }
+  else
   {
     document.getElementById('heading').innerHTML = "Enter your roman number"
+  }
+});
 
-    conv.addEventListener('click', function(e){
-    
+conv.addEventListener("click", function (e) {
+  if (count % 2 === 0) {
+    console.log("hi");
+    var deci = document.getElementById("decimal").value;
+    if (/^\d+$/.test(deci)) {
+      let romanNumObj = {
+        i: 1,
+        iv: 4,
+        v: 5,
+        ix: 9,
+        x: 10,
+        xl: 40,
+        l: 50,
+        xc: 90,
+        c: 100,
+        cd: 400,
+        d: 500,
+        cm: 900,
+        m: 1000,
+        mV: 4000,
+        V: 5000,
+        mX: 9000,
+        X: 10000,
+        XL: 40000,
+        L: 50000,
+        XC: 90000,
+        C: 100000,
+        cd: 400000,
+        D: 500000,
+        CM: 900000,
+        M: 1000000,
+      };
+
+      let romanKeys = Object.keys(romanNumObj);
+      let romanValues = Object.values(romanNumObj);
+
+      let romanNumArr = [];
+
+      for (let i = romanValues.length - 1; i >= 0; i--) {
+        // what I did was from the bottom of the object to subtract with number
+        // so greatest numbers subtract the given num
+        while (deci - romanValues[i] >= 0) {
+          deci = deci - romanValues[i];
+          romanNumArr.push(romanKeys[i]);
+          console.log(romanValues[i], romanKeys[i]);
+        }
+      }
+      console.log(romanNumArr.join(""));
+      var roman = romanNumArr.join("");
+      document.getElementById("roman").value = roman;
+    }
+    /*else
+        {
+          alert("Please enter a valid number")
+          document.getElementById("decimal").value = ""
+          document.getElementById("roman").value = ""
+        }*/
+  } 
+  
+  else {
+    console.log("nope");
+
     let input = document.getElementById("decimal").value
-      function x(){
-        return input = input.replace("IV", 'q').replace('IX', 'w').replace("XL", 'e').replace('XC', 'r').replace("CD", 't').replace('CM', 'y').replace("M_V", 'u').replace('M_X', 'o').replace("_X_L", 'p').replace('_X_C', 'k').replace("_C_D", 'j').replace('_C_M', 'h')}
-          x(input)
+      function changeExceptions(){
+        return input = input.replace("iv", 'q').replace('ix', 'w').replace("xl", 'e').replace('xc', 'r').replace("cd", 't').replace('cm', 'y').replace("mV", 'u').replace('mX', 'o').replace("XL", 'p').replace('XC', 'k').replace("CD", 'j').replace('CM', 'h')}
+          changeExceptions(input)
 
           let romanNumObj = {
-              "I": 1,
+              "i": 1,
               "q": 4,
-              "V": 5,
+              "v": 5,
               "w": 9,
-              "X": 10,
+              "x": 10,
               "e": 40,
-              "L": 50,
+              "l": 50,
               "r": 90,
-              "C": 100,
+              "c": 100,
               "t": 400,
-              "D": 500,
+              "d": 500,
               "y": 900,
-              "M": 1000,
+              "m": 1000,
               "u": 4000,
-              "v": 5000,
+              "V": 5000,
               "o": 9000,
-              "x":10000,
+              "X":10000,
               "p":40000,
-              "l":50000,
+              "L":50000,
               "k":90000,
-              "c":100000,
+              "C":100000,
               "j":400000,
-              "d":500000,
+              "D":500000,
               "h":900000,
-              "m":1000000  
+              "M":1000000  
             }
 
             let romNums =[]
@@ -71,80 +135,11 @@ function reverse()
           }
           console.log(result)
           document.getElementById("roman").value = result
-
-    })
   }
-
-  else
-  {
-    document.getElementById('heading').innerHTML = "Enter your number"
-  }
-
-}
+});
 
 
-conv.addEventListener("click", function(e) {
-    var deci = document.getElementById("decimal").value
-    if(/^\d+$/.test(deci) && count%2==1)
-    {
-      let romanNumObj = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "XL": 40,
-        "L": 50,
-        "XC": 90,
-        "C": 100,
-        "CD": 400,
-        "D": 500,
-        "CM": 900,
-        "M": 1000,
-        "Mv": 4000,
-        "v": 5000,
-        "Mx": 9000,
-        "x":10000,
-        "xl":40000,
-        "l":50000,
-        "xc":90000,
-        "c":100000,
-        "cd":400000,
-        "d":500000,
-        "cm":900000,
-        "m":1000000  
-      }
-      
-      let romanKeys = Object.keys(romanNumObj)
-      let romanValues = Object.values(romanNumObj)
-      
-      let romanNumArr = []
-      
-      for (let i=romanValues.length-1; i>=0; i--) 
-      {
-        // what I did was from the bottom of the object to subtract with number
-        // so greatest numbers subtract the given num
-        while (deci - romanValues[i] >= 0) 
-        {
-          deci = deci - romanValues[i]
-          romanNumArr.push(romanKeys[i])
-          console.log(romanValues[i], romanKeys[i])
-        }
-      }
-      console.log(romanNumArr.join(""))
-      var roman = romanNumArr.join("")
-      document.getElementById("roman").value = roman
-    }
-    else
-    {
-      /*alert("Please enter a valid number")
-      document.getElementById("decimal").value = ""
-      document.getElementById("roman").value = ""*/
-    }
-  })
-
-var reset = document.getElementById("reset")
 reset.addEventListener("click", function(e) {
-    document.getElementById("decimal").value = ""
-    document.getElementById("roman").value = ""
+  document.getElementById("decimal").value = ""
+  document.getElementById("roman").value = ""
 })
